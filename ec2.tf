c916a7ec27cfa1c206d884f8d1744ec3dcb9ae13
jmdprasad123 <98645028+jmdprasad123@users.noreply.github.com>
2022-02-04 23:33:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web1" {
-  ami                    = "ami-03fa4afc89e4a8a09"
+  ami                    = "ami-0651f1bab1a933ae5"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]

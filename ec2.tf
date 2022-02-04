10405a69373d546f1d24326db329d56cd928d0f6
jmdprasad123 <98645028+jmdprasad123@users.noreply.github.com>
2022-02-04 23:33:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web1" {
+resource "aws_instance" "web2" {
   ami                    = "ami-0651f1bab1a933ae5"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public[0].id
